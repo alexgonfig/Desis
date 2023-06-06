678791b667cd642abdb4f1acc55a5138d23bdc78
alexgonfig <alexgknot@gmail.com>
2023-06-06 06:04:45 -0400

    Update config.php

--- backend/config.php
+++ backend/config.php
@@ -1,7 +1,7 @@
 <?php 
 
-const DB_HOST = 'localhost';
-const DB_NAME = 'desis_test';
-const DB_USER = 'root';
-const DB_PASS = '';
+const DB_HOST = 'localhost'; 	//dirección del host
+const DB_NAME = 'desis_test';	//nombre de la base de datos
+const DB_USER = 'root'; 		  //credencial nombre de usuario de la base de datos
+const DB_PASS = ''; 			    // credencial contraseña de usuario de la base de datos
 const CONNECT = 'mysql:host='.DB_HOST.';dbname='.DB_NAME;
